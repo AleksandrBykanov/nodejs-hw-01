dc616475ec5fb8263a18a80144706a843741d82d
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-09-28 16:48:44 +0300

    make generate

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -2,9 +2,14 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from "node:fs/promises";
 
 export const readContacts = async () => {
-  const read = await fs.readFile(PATH_DB, "utf-8");
-  const data = JSON.parse(read);
-  return data;
+  try {
+    const read = await fs.readFile(PATH_DB, "utf-8");
+    const data = JSON.parse(read);
+    return data;
+  }
+  catch (error) {
+    console.log(error);
+  }
 };
 
 
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -2,9 +2,8 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from "node:fs/promises";
 
 export const writeContacts = async (updatedContacts) => {
-  const data = JSON.stringify(updatedContacts);
   try {
-    await fs.writeFile(PATH_DB, data, "utf8");
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, undefined, 2), "utf8");
   }
   catch (error) {
     console.log(error);
