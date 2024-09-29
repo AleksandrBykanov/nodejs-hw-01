419e16d43cb4a42600bc203ca4335ad5bddb3e2b
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-09-29 17:30:06 +0300

    make removeLast

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+  try {
+    const allContacts = await readContacts();
+    const newContacts = allContacts.slice(0, -1);
+    return writeContacts(newContacts);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
