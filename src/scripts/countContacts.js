7fb4b1e5103ad7818f2253ecd0c452799ab6b52f
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-09-29 16:48:18 +0300

    make count

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,14 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+  try {
+    const allContacts = await readContacts();
+    const count = allContacts.length;
+    return count;
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await countContacts());
