f76b4f9ea748bf446cb331c3a2c45a9d6914992a
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-09-29 16:27:32 +0300

    make addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+  try {
+    const oldContacts = await readContacts();
+    const newContacts = [createFakeContact()];
+    const updatedContacts = [...oldContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -4,12 +4,12 @@ import { writeContacts } from "../utils/writeContacts.js";
 
 const generateContacts = async (number) => {
   try {
-    const contacts = await readContacts();
+    const oldContacts = await readContacts();
     const newContacts = [];
     for (let i = 0; i < number; i++) {
       newContacts.push(createFakeContact());
     }
-    const updatedContacts = [...contacts, ...newContacts];
+    const updatedContacts = [...oldContacts, ...newContacts];
     await writeContacts(updatedContacts);
   }
   catch (error) {
